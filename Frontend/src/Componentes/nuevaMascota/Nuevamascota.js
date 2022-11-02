b5d1aaa0588b1837f55e8632cf3f385c90c3dea5
DRAC-09 <allan.estrada9@gmail.com>
2022-11-02 14:27:00 -0600

    Añadido Boton para añadir una mascota a la base de datos

--- Frontend/src/Componentes/nuevaMascota/Nuevamascota.js
+++ Frontend/src/Componentes/nuevaMascota/Nuevamascota.js
@@ -9,7 +9,11 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import './nuevamascota.css';
+import photo from '../../Fotos/pet2.png'
 // import "bootstrap/dist/css/bootstrap.min.css";
+
+
+
 const theme = createTheme({
   palette: {
     primary: {
@@ -26,6 +30,8 @@ const theme = createTheme({
     },
   },
 });
+
+
 function Copyright(props) {
   return (
     <Typography variant="body2" color="text.secondary" align="center" background-attachment= "fixed" {...props} >
@@ -38,193 +44,158 @@ function Copyright(props) {
     </Typography>
   );
 }
+
+
 export default function Nuevamascota() {
-  const [images, setimages] = useState([]);
+  // Aqui se guardara el base64 de la imagen de la mascota
+  const [imagen, setimagen] = useState("");
+  let abrirModal = false
 
+
+  // Capturar Imagen y convertirla a Base64
   const changeInput = (e) => {
-    //esto es el indice que se le dará a cada imagen, a partir del indice de la ultima foto
-    let indexImg;
+    let reader = new FileReader();
+    reader.readAsDataURL(e.target.files[0])
+    reader.onload = () => {
+      // previsualizacion (reader.result)
+      // console.log(reader.result);
+    setimagen(reader.result)
+    }    };
 
-    //aquí evaluamos si ya hay imagenes antes de este input, para saber en dónde debe empezar el index del proximo array
-    if (images.length > 0) {
-      indexImg = images[images.length - 1].index + 1;
-    } else {
-      indexImg = 0;
-    }
-
-    let newImgsToState = readmultifiles(e, indexImg);
-    let newImgsState = [...images, ...newImgsToState];
-    setimages(newImgsState);
-
-    console.log(newImgsState);
-  };
-
-  function readmultifiles(e, indexInicial) {
-    const files = e.currentTarget.files;
-
-    //el array con las imagenes nuevas
-    const arrayImages = [];
-
-    Object.keys(files).forEach((i) => {
-      const file = files[i];
-
-      let url = URL.createObjectURL(file);
-
-      //console.log(file);
-      arrayImages.push({
-        index: indexInicial,
-        name: file.name,
-        url,
-        file
-      });
-
-      indexInicial++;
-    });
-
-    //despues de haber concluido el ciclo retornamos las nuevas imagenes
-    return arrayImages;
-  }
-
-  function deleteImg(indice) {
-    //console.log("borrar img " + indice);
-
-    const newImgs = images.filter(function (element) {
-      return element.index !== indice;
-    });
-    console.log(newImgs);
-    setimages(newImgs);
-  }
-
+  // Captura de la informacion d ela mascota y Conexion a la base de datos
   const handleSubmit = (event) => {
-    event.preventDefault();
     const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
+    const mascota = {
+      nombre: data.get('nombre'),
+      edad: data.get('edad'),
+      raza: data.get('raza'),
+      sexo: data.get('sexo'),
+      descripcion: data.get('descripcion'),
+      imagen: imagen
+    }
+    // console.log(mascota)
+    
+    fetch("http://localhost:7777/mascota/agregar", {
+      method: 'POST',
+      body: JSON.stringify(mascota),
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+      })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+      alert(data.mensaje)
+      window.location.href = "./nuevamascota"; 
+    })
+    .catch(res => console.log(res));
+    event.preventDefault();
   };
+
   return (
     <div className="containe-fluid div-crearmascota" bgcolor="transparent">
-    <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
+      <ThemeProvider theme={theme}>
+        <Container component="main" maxWidth="xs">
+          <CssBaseline />
+          <Box
+            sx={{
+              marginTop: 8,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
           <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-            <Grid container spacing={2} >
-              <Grid item xs={12}>
-                <TextField
-                  autoComplete="given-name"
-                  variant='filled'
-                  name="Name"
-                  required
-                  fullWidth
-                  id="Nombre"
-                  label="Nombre"
-                  autoFocus
-                />
-              </Grid>
-              <Grid item xs={12} >
-                <TextField 
-                  variant='filled'
-                  name="age"
-                  required
-                  fullWidth
-                  id="Edad"
-                  label="Edad"
-                  autoFocus
-                />
-              </Grid>
-              <Grid item xs={12} >
-                <TextField 
-                  variant='filled'
-                  name="race"
-                  required
-                  fullWidth
-                  id="Raza"
-                  label="Raza"
-                  autoFocus
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField 
-                  variant='filled'
-                  name="sexo"
-                  required
-                  fullWidth
-                  id="sexo"
-                  label="sexo"
-                  autoFocus
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  autoComplete="given-name"
-                  variant='filled'
-                  name="description"
-                  required
-                  multiline
-                  rows={1}
-                  fullWidth
-                  id="Descripción"
-                  label="Descripción"
-                  autoFocus
-                />
-            </Grid>
-            <Grid  item xs={12} >
-            <div className="container-fluid">
-      <br></br>
+              <Grid container spacing={2} >
+                <Grid item xs={12}>
+                  <TextField
+                    autoComplete="given-name"
+                    variant='filled'
+                    name="nombre"
+                    required
+                    fullWidth
+                    id="Nombre"
+                    label="Nombre"
+                    autoFocus
+                  />
+                </Grid>
+                <Grid item xs={12} >
+                  <TextField 
+                    variant='filled'
+                    name="edad"
+                    required
+                    fullWidth
+                    id="Edad"
+                    label="Edad"
+                    autoFocus
+                  />
+                </Grid>
+                <Grid item xs={12} >
+                  <TextField 
+                    variant='filled'
+                    name="raza"
+                    required
+                    fullWidth
+                    id="Raza"
+                    label="Raza"
+                    autoFocus
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField 
+                    variant='filled'
+                    name="sexo"
+                    required
+                    fullWidth
+                    id="sexo"
+                    label="sexo"
+                    autoFocus
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    autoComplete="given-name"
+                    variant='filled'
+                    name="descripcion"
+                    required
+                    multiline
+                    rows={1}
+                    fullWidth
+                    id="Descripción"
+                    label="Descripción"
+                    autoFocus
+                  />
+                </Grid>
+              <Grid  item xs={12} >
+              <div className="container-fluid">
+        <br></br>
+    
 
-      {/* INPUT IMAGES */}
-      <label className="btn btn-warning">
-        <span>Seleccionar imagen </span>
-        <input hidden type="file" multiple onChange={changeInput}></input>
-      </label>
+        {/* VIEW IMAGES */}
+        <div className="foto">
+            <img src={imagen || photo}></img><br></br>
+            {/* INPUT IMAGES */}
+            <label className="btn btn-warning">
+              <span>Añadir Foto de Perfil </span>
+              <input hidden type="file" multiple onChange={changeInput}></input>
+            </label>
+        </div>
 
-      {/* VIEW IMAGES */}
-      <div className="row">
-        {images.map((imagen) => (
-          <div className="col-6 col-sm-4 col-lg-3 square" key={imagen.index}>
-            <div className="content_img">
-              <button
-                className="position-absolute btn btn-danger"
-                onClick={deleteImg.bind(this, imagen.index)}
-              >
-                x
-              </button>
-              <img
-                alt="algo"
-                src={imagen.url}
-                data-toggle="modal"
-                data-target="#ModalPreViewImg"
-                className="img-responsive"
-              ></img>
-            </div>
-          </div>
-        ))}
       </div>
-    </div>
               </Grid>
-            </Grid>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Crear Mascota
-            </Button>
+              </Grid>
+              <Button 
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >Añadir Mascota</Button>
+            </Box>
           </Box>
-        </Box>
-      </Container>
-      <Copyright sx={{ mt: 5 } } />
-    </ThemeProvider>
+        </Container>
+        <Copyright sx={{ mt: 5 } } />
+      </ThemeProvider>
     </div>
-  );
+  );  
 }
