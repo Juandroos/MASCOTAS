afde3393edac3c4dd290dfd83528b99a1de77572
DRAC-09 <allan.estrada9@gmail.com>
2022-11-09 21:43:11 -0600

    Actualizacion esquema tabla (MASCOTAS)(Campo: fechaActualizacion)

--- Backend/models/mascota.js
+++ Backend/models/mascota.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
 
 var esquema = new mongoose.Schema({
-          nombre:        {type: String, required: true},
-          edad:          {type: String, required: true},
-          raza:          {type: String, required: true},
-          sexo:          {type: String, required: true},
-          descripcion:   {type: String, required: true},
-          imagen:        {type: String}
+          nombre:              {type: String, required: true},
+          edad:                {type: String, required: true},
+          raza:                {type: String, required: true},
+          sexo:                {type: String, required: true},
+          descripcion:         {type: String, required: true},
+          imagen:              {type: String},
+          fechaActualizacion:  {type: Date, default: Date.now}
 });
 
 module.exports = mongoose.model('mascotas', esquema);
