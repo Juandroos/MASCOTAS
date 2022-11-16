c5df5e2390a4130f6bad26e2382633d3cc0ff3c0
ASevilla25 <alfonsosevilla_25@hotmail.com>
2022-11-16 15:47:09 -0600

    campo fecha actualizacion

--- Backend/models/actividad.js
+++ Backend/models/actividad.js
@@ -1,15 +1,16 @@
 const mongoose = require('mongoose');
 
 var esquema = new mongoose.Schema({
-          tituloevento:         {type: String, required: true},
-          resumen:              {type: String, required: true},
-          direccion:            {type: String, required: true},
-          descripcion:          {type: String, required: true},
-          fechaevento:          {type: String, required: true},
-          telefono:             {type: String, required: true},
-          organizador:          {type: String, required: true},
-          correoelectronico:    {type: String, required: true},
-          beneficio:            {type: String, required: true}
+          titulo: {type: String, required: true},
+          resumen: {type: String, required: true},
+          direccion: {type: String, required: true},
+          descripcion: {type: String, required: true},
+          fecha: {type: String, required: true},
+          celular: {type: String, required: true},
+          organizador: {type: String, required: true},
+          correoElectronico: {type: String, required: true},
+          beneficio: {type: String, required: true},
+          fechaActualizacion:  {type: Date, default: Date.now}
 });
 
 module.exports = mongoose.model('actividades', esquema);
