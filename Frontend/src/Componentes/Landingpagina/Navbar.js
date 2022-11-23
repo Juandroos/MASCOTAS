c3439d2086ec5d0d09c730bf5e397f0744aeead4
DRAC-09 <allan.estrada9@gmail.com>
2022-11-23 15:30:38 -0600

    Modificado Navbar con los datos del Usuario

--- Frontend/src/Componentes/Landingpagina/Navbar.js
+++ Frontend/src/Componentes/Landingpagina/Navbar.js
@@ -7,14 +7,18 @@ import foto from  '../../Fotos/user.png'
 // import UserContext from '../../state/user/UserContext';
 
 function Navbar() {
+
   useEffect(() => {
     userLS();
     showButton();
+    // console.log(fotoPerfil)
   }, []);
 
   const [click, setClick] = useState(false);
   const [button, setButton] = useState(true);
   const [userName, setuserName] = useState();
+  const [fotoPerfil, setfotoPerfil] = useState();
+  // const { user, getUser } = useContext(UserContext)
   
 
   const handleClick = () => setClick(!click);
@@ -28,27 +32,39 @@ function Navbar() {
     }
   };
 
-  // const { user, getUser } = useContext(UserContext)
 
+  // Redireccionar dependiendo si el usuario esta o no logeado
   const loadUser = () =>{
-    // console.log()
-    if (localStorage.getItem('user') == null){window.location.href = "./login";}
-    if (localStorage.getItem('user') != null){window.location.href = "./inicio";}
+    if (userName == null){window.location.href = "./login";}
+    if (userName != null){window.location.href = "./inicio";}
   }
 
   const userLS = () => {
-    // console.log('navbar')
-    if (localStorage.getItem('user') == null){
-      setuserName('Usuario')
-      // console.log(userActual.nombre)
-    }else {
-      const userActual = JSON.parse(localStorage.getItem('user'));
-      setuserName(userActual.nombre)
-    }
+      const id = JSON.parse(localStorage.getItem('user'))
+      // console.log(fotoPerfil)
+
+    fetch(`http://localhost:7777/cliente/${id}`, {
+          method: 'GET',
+          headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json'
+          }
+    })
+      .then(res => res.json())
+      .then(data => {
+          setuserName(data[0].nombre)
+          setfotoPerfil(data[0].fotoPerfil)
+          // console.log(data[0].fotoPerfil)
+      })
+      .catch(res => console.log(res));
+
+    // if (localStorage.getItem('user') !== null){
+    //   const userActual = JSON.parse(localStorage.getItem('user'));
+    //   setuserName(userActual.nombre)
+    //   setfotoPerfil(userActual.fotoPerfil)
+    // }
   }
   
-
-
   window.addEventListener('resize', showButton);
 
   return (
@@ -56,8 +72,6 @@ function Navbar() {
       <nav className='navbar'>
         <div className='navbar-container'>
           <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
-            {/* NUEVO HOGAR */}
-            {/* <i class='fab fa-typo3' /> */}
             <img src={logo}></img>
           </Link>
           <div className='menu-icon' onClick={handleClick}>
@@ -89,12 +103,33 @@ function Navbar() {
             </li>
             <li className='nav-item user'>
               <Link
-                // to='/login'
                 className='nav-links'
                 onClick={ () => loadUser()}
               >
-                <img src={ foto} className='photo-perfil'></img>
-                {userName}
+                {(() => {
+                    if (userName === undefined){
+                      return (
+                        <div>
+                          <img src={foto} className='photo-perfil'></img>
+                          Usuario
+                        </div>)
+                    }
+                    else if (userName !== undefined) {
+                      if (fotoPerfil === undefined){
+                        return (
+                          <div>
+                            <img src={foto} className='photo-perfil'></img>
+                            {userName}
+                          </div>)
+                      }else{
+                        return(
+                          <div>
+                            <img src={fotoPerfil} className='photo-perfil'></img>
+                            {userName}
+                          </div>)
+                      }
+                    }
+                })()}
               </Link>
             </li>
           </ul>
